c341857939ed2a92e4abe9f1697fe55c8d5c2122
Anders Bjerner <abjerner@skybrud.dk>
2016-02-19 10:01:54 +0100

    New version :D

--- src/Skybrud.Umbraco.BorgerDk/Properties/AssemblyInfo.cs
+++ src/Skybrud.Umbraco.BorgerDk/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Skybrud.Umbraco.BorgerDk")]
-[assembly: AssemblyDescription("Integration mellem Skybrud.BorgerDk og Umbraco 7")]
+[assembly: AssemblyDescription("Skybrud.Umbraco.BorgerDk is an integration between our Skybrud.BorgerDk package and Umbraco7. The package gives editors the option to insert Borger.dk articles as part of their content in their own pages in Umbraco.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skybrud.dk")]
 [assembly: AssemblyProduct("Skybrud.Umbraco.BorgerDk")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyInformationalVersion("2.0.1")]
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyInformationalVersion("2.0.2")]
--- src/Skybrud.Umbraco.BorgerDk/Properties/AssemblyInfoGenerated.cs
+++ src/Skybrud.Umbraco.BorgerDk/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.17.5")]
+[assembly: AssemblyFileVersion("0.0.17.6")]
 
