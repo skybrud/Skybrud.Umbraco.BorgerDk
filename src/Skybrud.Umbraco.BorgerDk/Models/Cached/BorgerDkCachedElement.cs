8f2d8810c234ee7b256102660dea750be9c011af
Anders Bjerner <abjerner@skybrud.dk>
2016-07-12 13:40:12 +0200

    Added methods to "BorgerDkCachedElement" to check whether the element matches a specific element type

--- src/Skybrud.Umbraco.BorgerDk/Models/Cached/BorgerDkCachedElement.cs
+++ src/Skybrud.Umbraco.BorgerDk/Models/Cached/BorgerDkCachedElement.cs
@@ -17,6 +17,25 @@ namespace Skybrud.Umbraco.BorgerDk.Models.Cached {
         /// </summary>
         public string Alias { get; protected set; }
 
+        /// <summary>
+        /// Gets whether the instance is of type <code>T</code>.
+        /// </summary>
+        /// <typeparam name="T">The intended type.</typeparam>
+        public bool IsOfType<T>() where T : BorgerDkCachedElement {
+            T casted = this as T;
+            return casted != null;
+        }
+
+        /// <summary>
+        /// Gets whether the instance is of type <code>T</code>.
+        /// </summary>
+        /// <typeparam name="T">The intended type.</typeparam>
+        /// <param name="value">A reference to the casted value.</param>
+        public bool IsOfType<T>(out T value) where T : BorgerDkCachedElement {
+            value = this as T;
+            return value != null;
+        }
+
     }
 
 }
