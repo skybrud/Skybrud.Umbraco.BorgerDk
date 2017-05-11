704026f9ca2d298f39f53aea6efe70942e30cbb2
Anders Bjerner <abjerner@skybrud.dk>
2017-05-11 20:30:42 +0200

    Updated the dependency for Skybrud.Umbraco.GridData

--- src/Skybrud.Umbraco.BorgerDk/Grid/Converters/BorgerDkGridConverter.cs
+++ src/Skybrud.Umbraco.BorgerDk/Grid/Converters/BorgerDkGridConverter.cs
@@ -1,8 +1,6 @@
-using System;
 using Newtonsoft.Json.Linq;
 using Skybrud.Umbraco.BorgerDk.Grid.Values;
 using Skybrud.Umbraco.GridData;
-using Skybrud.Umbraco.GridData.Extensions.Json;
 using Skybrud.Umbraco.GridData.Interfaces;
 using Skybrud.Umbraco.GridData.Rendering;
 
