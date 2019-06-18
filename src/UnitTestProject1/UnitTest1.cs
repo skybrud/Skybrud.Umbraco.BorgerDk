e22723041ffbf2d492cbe43cdefdf7d51183f41b
Anders Bjerner <abjerner@skybrud.dk>
2019-06-18 11:28:08 +0200

    Added new test project

---
+++ src/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Umbraco.BorgerDk.Models;
+
+namespace UnitTestProject1 {
+
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void TestMethod1() {
+
+            string json = "{\"id\":0,\"url\":\"\",\"selected\":[],\"customTitle\":{\"type\":\"article\",\"value\":\"\"}}";
+
+            BorgerDkArticleSelection selection = BorgerDkArticleSelection.Deserialize(json);
+
+            Assert.AreEqual(0, selection.Id);
+            Assert.AreEqual(string.Empty, selection.Url);
+            Assert.AreEqual(0, selection.Selected.Length);
+            Assert.IsNull(selection.Municipality);
+            Assert.IsNull(selection.Municipality);
+
+        }
+
+    }
+
+}
