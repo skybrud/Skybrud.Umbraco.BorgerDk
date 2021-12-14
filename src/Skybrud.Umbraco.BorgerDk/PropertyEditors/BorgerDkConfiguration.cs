ec3bdbeb175a1ca4fccdadce7d4a9d9914097908
Anders Bjerner <abjerner@skybrud.dk>
2021-12-14 15:10:10 +0100

    Configutration field view URLs shouldn't start with a tilde character

--- src/Skybrud.Umbraco.BorgerDk/PropertyEditors/BorgerDkConfiguration.cs
+++ src/Skybrud.Umbraco.BorgerDk/PropertyEditors/BorgerDkConfiguration.cs
@@ -5,10 +5,10 @@ namespace Skybrud.Umbraco.BorgerDk.PropertyEditors {
     
     public class BorgerDkConfiguration {
 
-        [ConfigurationField("municipality", "Municipality", "~/App_Plugins/Skybrud.BorgerDk/Views/Municipality.html", Description = "Select the municipality to be used.")]
+        [ConfigurationField("municipality", "Municipality", "/App_Plugins/Skybrud.BorgerDk/Views/Municipality.html", Description = "Select the municipality to be used.")]
         public BorgerDkMunicipality Municipality { get; set; }
 
-        [ConfigurationField("allowedTypes", "Allowed types", "~/App_Plugins/Skybrud.BorgerDk/Views/AllowedTypes.html", Description = "Select the element types that should be allowed.")]
+        [ConfigurationField("allowedTypes", "Allowed types", "/App_Plugins/Skybrud.BorgerDk/Views/AllowedTypes.html", Description = "Select the element types that should be allowed.")]
         public string[] AllowedTypes { get; set; }
 
     }
